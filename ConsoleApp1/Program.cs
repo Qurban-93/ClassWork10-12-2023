df8ef4294453c27f49e9c51ce367254c3a1fd9b4
Qurban-93 <qurban231293@gmail.com>
2023-12-10 14:42:47 +0400

    first

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿bool hasUp= false, hasNum = false;
+
+l1:
+Console.Write("Enter your pass: ");
+string pass = Console.ReadLine();
+
+foreach (var letter in pass)
+{
+    if (Char.IsUpper(letter))
+    {
+        hasUp = true;
+    }
+    else if (Char.IsNumber(letter))
+    {
+        hasNum = true;
+    }
+}
+if(hasUp && hasNum)
+{
+    Console.WriteLine("Good pass");
+}
+else
+{
+    goto l1;
+}
